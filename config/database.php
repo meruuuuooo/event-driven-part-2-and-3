a6de49482ecc8ccec2e084503e50c76ed13e373a
meruuuuooo <merudeibitto@gmail.com>
2024-05-01 15:59:16 +0800

    Fix typo in database name in config/database.php

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ $servername = "localhost";
 $username = "root";
 $password = ""; // Default Password: Blank | Empty
 
-$database_name = "db_test"; 
+$database_name = "db_tes"; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database_name);
